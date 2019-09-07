3d56a2efbf332c913d016f904e288ff443f04ded
Marcin Stenka <mastenka@wp.pl>
2019-09-07 16:12:15 +0200

    Updates

---
+++ precache-manifest.46611a5b3342399c2692f33493680d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700e9f44d92ce803cf8c1c694664362c",
+    "url": "/lyric-sten/index.html"
+  },
+  {
+    "revision": "bdb6acf23173a26bf836",
+    "url": "/lyric-sten/static/css/main.8e2222c0.chunk.css"
+  },
+  {
+    "revision": "eac531eeeac16e7a2a5f",
+    "url": "/lyric-sten/static/js/2.230df37a.chunk.js"
+  },
+  {
+    "revision": "bdb6acf23173a26bf836",
+    "url": "/lyric-sten/static/js/main.cff9c512.chunk.js"
+  },
+  {
+    "revision": "583afebe1bcd86638776",
+    "url": "/lyric-sten/static/js/runtime~main.c8dfe4fa.js"
+  }
+]);
